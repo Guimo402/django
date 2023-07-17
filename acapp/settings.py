e6c19c39b6df2f096fea2abb5bfaa812e81b42ff
WLF <guimo402@outlook.com>
2023-07-17 23:20:47 +0800

    modify allowed host

--- acapp/settings.py
+++ acapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-abpusk#ghxg0u41q6oi6c5lhe+pjae2k&fh%nxk^m%)_v#n5hl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["39.101.1.113"]
 
 
 # Application definition
