39b5b2149971f2bf23edb5c73c9de8248120d3da
guimo <guimo402@outlook.com>
2023-10-08 22:45:19 +0800

    finish naive login system

--- game/urls/settings/index.py
+++ game/urls/settings/index.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from game.views.settings.getinfo import getinfo
+from game.views.settings.login import signin
+from game.views.settings.logout import signout
+from game.views.settings.register import register
 
 
 urlpatterns = [
+    # 参数：设置访问路径，调用函数名，这条路径的名字
+    path("getinfo/", getinfo, name="settings_getinfo"),
+    path("login/", signin, name="settings_login"),
+    path("logout/", signout, name="settings_logout"),
+    path("register/", register, name="settings_register"),
 ]
