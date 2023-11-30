4a22c85aa1d71e5b2999a1fcc5179bcbaf7443c3
WLF <guimo402@outlook.com>
2023-11-30 16:52:30 +0800

    implement web acwing oauth2 login

--- game/models/player/player.py
+++ game/models/player/player.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.URLField(max_length=256, blank=True)
+    openid = models.CharField(default="", max_length=50, blank=True, null=True)
     
     def __str__(self): # 这个函数的作用是用户名是什么
         return str(self.user)
---
+++ game/views/settings/acwing/web/apply_code.py
@@ -0,0 +1,25 @@
+from django.http import JsonResponse
+from urllib.parse import quote  # 用来对url进行封装，替换特殊字符
+from random import randint
+from django.core.cache import cache
+
+def get_state():
+    res = ""
+    for i in range(8):
+        res += str(randint(0, 9))
+    return res
+
+def apply_code(request):
+    appid = "5491"
+    redirect_uri = quote("https://app5491.acapp.acwing.com.cn/settings/acwing/web/receive_code/")
+    scope = "userinfo"
+    state = get_state()
+
+    cache.set(state, True, 3200) # 有效期两个小时
+
+    apply_code_url = "https://www.acwing.com/third_party/api/oauth2/web/authorize/"
+
+    return JsonResponse({
+        'result': "success",
+        'apply_code_url': apply_code_url + "?appid=%s&redirect_uri=%s&scope=%s&state=%s" % (appid, redirect_uri, scope, state)
+    })
